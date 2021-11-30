d9df0364f72f96fd91e24054ca6664abf6b8aa04
Udontknowderek <92171841+Udontknowderek@users.noreply.github.com>
2021-11-30 15:18:51 +0530

    Create fibobyfor.c

---
+++ fibobyfor.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int i, n;
+    int a = 1, b = 1;
+     int c = a + b; 
+    printf("enter the number of terms");
+    scanf("%d", &n);
+    printf("%d, %d, ", a, b);
+    for (i = 3; i <=n; i++){
+        printf("%d, " , c);
+        a = b;
+        b = c; 
+        c = a + b;
+    }
+    return 0; 
+    
+}
